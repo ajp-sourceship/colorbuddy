5eba47dcee6fb2eea3647e4f8cc30c64c4cb45a7
sourceship <artificalpeter@gmail.com>
2021-06-30 21:58:32 -0500

    undid typscript changes as it broke functionality

--- src/Chart.js
+++ src/Chart.js
@@ -4,10 +4,10 @@ import { Header } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default Chart = (props) => {
-  return (
+  return ( // flatlist is better practice
     <ScrollView style={styles.container}>
-      {props.colors.map(color => {
-        return <TouchableOpacity
+      {props.colors.map((color, index) => {
+        return <TouchableOpacity id={index}
           onPress={() => props.selectColor(color) }
            style={{ backgroundColor: 'white', height: 80, margin:10, borderRadius:6, padding:2 }}>
           < View style={{ height: '80%', backgroundColor: color.HexString, borderColor: 'white', borderRadius:6,  }}>
